55585b848573474e9bfe7c302ba2f74add896d54
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-02-03 13:46:28 +0800

    create repository

---
+++ backend/backend/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace backend.Repositories.Implementation
+{
+    public class CategoryRepository
+    {
+    }
+}
---
+++ backend/backend/Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using backend.Models.Domain;
+
+namespace backend.Repositories.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateAsync(Category category);
+    }
+}
