6e2100763f023eced36fe7c70d3a56307bc57271
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-02-03 13:46:15 +0800

    create DbContext and connect with DB

---
+++ backend/backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using backend.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+    }
+}
---
+++ backend/backend/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace backend.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder();
+            builder.UseSqlServer("Server=DESKTOP-MR5OABM\\SQLEXPRESS; Database=BlogAppDb; TrustServerCertificate=True; Trusted_Connection=True;");
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
