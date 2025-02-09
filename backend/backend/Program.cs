cfe96d1effb4c6218647d2c931c551b2e23df947
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2025-02-09 13:12:35 +0800

    fix http accessor bug - backend

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor(); // http context accessor
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +24,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // need to write this after created implementation and interface
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>(); // add dependency injection and create new instance in every HTTP request
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
+builder.Services.AddScoped<IImageRepository, ImageRepository>();
 
 var app = builder.Build();
 
